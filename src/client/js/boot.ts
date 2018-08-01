72e1af1cdf584f7188dae3b7c56a0b516996b657
zhangaz1 <zhangaz1@hotmail.com>
2018-08-01 19:08:29 +0800

    client file js to ts

--- src/client/js/boot.ts
+++ src/client/js/boot.ts
@@ -1,1 +1,31 @@
-import consts from './common/consts';
+import consts from './common/consts.js';
+
+boot();
+
+// return void (0);
+
+function boot() {
+	$('#snake').click(loadGameHander);
+}
+
+function loadGameHander() {
+	clearGame();
+	loadGame(this.id);
+}
+
+function loadGame(game) {
+	const gameModule = `./../games/${game}/boot.js`;
+	// 暂不支持await
+	// const module = await import(game)
+	// module.default();
+
+	import(gameModule)
+		.then(function (module) {
+			module.default();
+		});
+}
+
+function clearGame() {
+	consts.gameGround.empty();
+	consts.gameScripts.empty();
+}
