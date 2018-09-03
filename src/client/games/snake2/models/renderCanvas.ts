f86552b5977affa6acbc6c138426b41a78ec215c
zhangaz1 <zhangaz1@hotmail.com>
2018-09-03 17:27:30 +0800

    add renderCell and renderRactangle

--- src/client/games/snake2/models/renderCanvas.ts
+++ src/client/games/snake2/models/renderCanvas.ts
@@ -1,5 +1,9 @@
 import IRender from './../interfaces/render.js';
 
+import Colors from '../enums/colors.js';
+
+import Cell from './../types/cell.js';
+
 import Foods from './foods.js';
 import Food from './food.js';
 import Snake from './snake.js';
@@ -27,5 +31,23 @@ export default class RenderCanvas implements IRender {
 	public renderScore(score: Score): void { }
 
 
-	private renderCell() { }
+	private renderCell(cell: Cell, color: Colors) {
+		const config = this.renderConfig;
+		const borderSize = config.cellBorderSize;
+		const cellWidth = config.cellWidth;
+		const cellHeight = config.cellHeight;
+
+		const x = cell.x * cellWidth + borderSize;
+		const y = cell.y * cellHeight + borderSize;
+
+		const width = cellWidth - borderSize * 2;
+		const height = cellHeight - borderSize * 2;
+
+		this.renderRectangle(x, y, width, height, color);
+	}
+
+	private renderRectangle(x, y, width, height, color) {
+		this.context.fillStyle = color;
+		this.context.fillRect(x, y, width, height);
+	}
 }
