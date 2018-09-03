8e9488891ba2deebb5d7dbcd4df4c332d9c19ff1
zhangaz1 <zhangaz1@hotmail.com>
2018-09-03 19:47:48 +0800

    add rectangle

---
+++ src/client/games/snake2/interfaces/rectangle.ts
@@ -0,0 +1,8 @@
+import ICoordinate from './coordinate.js';
+import ISize from './size.js';
+import Colors from './../enums/colors.js';
+
+export default interface Rectangle extends ICoordinate, ISize {
+	borderSize: number;
+	borderColor: Colors;
+}
---
+++ src/client/games/snake2/models/rectangle.ts
@@ -0,0 +1,29 @@
+import IRectangle from './../interfaces/rectangle.js';
+import Colors from './../enums/colors.js';
+
+export default class Rectangle implements IRectangle {
+	public x: number;
+	public y: number;
+
+	public width: number;
+	public height: number;
+
+	public borderSize: number;
+	public borderColor: Colors;
+
+	constructor({
+		x = 0,
+		y = 0,
+		width = 0,
+		height = 0,
+		borderSize = 0,
+		borderColor = Colors.Black,
+	}) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.borderSize = borderSize;
+		this.borderColor = borderColor;
+	}
+}
