71fe5db95fa9980be587193afe301a1caed296ec
zhangaz1 <zhangaz1@hotmail.com>
2018-09-04 10:07:06 +0800

    add Colors.None

--- src/client/games/snake2/enums/colors.ts
+++ src/client/games/snake2/enums/colors.ts
@@ -2,9 +2,11 @@ const enum Colors {
 	Red = 'red',
 	Yellow = 'yellow',
 	Blue = 'blue',
-	Green= 'green',
+	Green = 'green',
 	Black = 'black',
 	White = 'white',
+
+	None = 'none',
 }
 
 export default Colors;
