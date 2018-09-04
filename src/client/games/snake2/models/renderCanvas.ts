469d72d44031a7a696f057528ec9fbcb6dccc9d6
zhangaz1 <zhangaz1@hotmail.com>
2018-09-04 10:24:55 +0800

    implaments renderCanvas

--- src/client/games/snake2/models/renderCanvas.ts
+++ src/client/games/snake2/models/renderCanvas.ts
@@ -1,6 +1,6 @@
 import IRender from './../interfaces/render.js';
 
-import Colors from '../enums/colors.js';
+import Colors from './../enums/colors.js';
 
 import Cell from './../types/cell.js';
 
@@ -8,7 +8,15 @@ import Foods from './foods.js';
 import Food from './food.js';
 import Snake from './snake.js';
 import Score from './score.js';
+import Rectangle from './rectangle.js';
 import RenderConfig from './renderConfig.js';
+
+class CellParamsObj {
+	cell: Cell;
+	fillColor?: Colors = null;
+	borderSize?: number = null;
+	borderColor?: Colors = null;
+}
 
 export default class RenderCanvas implements IRender {
 	private readonly context: CanvasRenderingContext2D;
@@ -21,33 +29,85 @@ export default class RenderCanvas implements IRender {
 	}
 
 	public renderVenue(): void {
-
+		const rectangle = this.getVenueRectangle();
+		this.drawRectangle(rectangle);
 	}
 
-	public renderFoods(foods: Foods): void { }
+	public renderFoods(foods: Foods): void {
+		foods.foods
+			.forEach(food => this.renderCell({
+				cell: food.position,
+				fillColor: food.color,
+			}));
+	}
 
-	public renderSnake(snake: Snake): void { }
+	public renderSnake(snake: Snake): void {
+		this.renderCell({
+			cell: snake.head,
+			fillColor: this.renderConfig.snakeHeadColor,
+		});
 
-	public renderScore(score: Score): void { }
+		snake.body.forEach(cell => this.renderCell({
+			cell,
+			fillColor: this.renderConfig.snakeBodyColor,
+		}));
+	}
 
+	public renderScore(score: Score): void {
+		console.log(JSON.stringify(score, null, 4));
+	}
 
-	private renderCell(cell: Cell, color: Colors) {
+	private getVenueRectangle() {
 		const config = this.renderConfig;
-		const borderSize = config.cellBorderSize;
+		const venueBorderSize = config.venueBorderSize;
+
+		return new Rectangle({
+			width: config.width * venueBorderSize * 2,
+			height: config.height + venueBorderSize * 2,
+			borderSize: venueBorderSize,
+			borderColor: config.venueBorderColor,
+			fillColor: config.groundColor,
+		});
+	}
+
+	private renderCell(cellParamsObj: CellParamsObj): void {
+		const rectangle = this.getCellRectangle(cellParamsObj);
+		this.drawRectangle(rectangle);
+	}
+
+	private getCellRectangle(cellParamsObj: CellParamsObj): Rectangle {
+		const config = this.renderConfig;
+		const borderSize = cellParamsObj.borderSize !== null ? cellParamsObj.borderSize : config.cellBorderSize;
 		const cellWidth = config.cellWidth;
 		const cellHeight = config.cellHeight;
+		const cell = cellParamsObj.cell;
 
-		const x = cell.x * cellWidth + borderSize;
-		const y = cell.y * cellHeight + borderSize;
-
-		const width = cellWidth - borderSize * 2;
-		const height = cellHeight - borderSize * 2;
-
-		this.renderRectangle(x, y, width, height, color);
+		return new Rectangle({
+			x: cell.x * cellWidth + borderSize,
+			y: cell.y * cellHeight + borderSize,
+			width: cellWidth - borderSize * 2,
+			height: cellHeight - borderSize * 2,
+			fillColor: cellParamsObj.fillColor,
+			borderSize,
+			borderColor: cellParamsObj.borderColor,
+		});
 	}
 
-	private renderRectangle(x, y, width, height, color) {
-		this.context.fillStyle = color;
-		this.context.fillRect(x, y, width, height);
+	private drawRectangle(rectangle: Rectangle) {
+		const context = this.context;
+		context.beginPath();
+		context.rect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+
+		if (rectangle.borderSize) {
+			context.lineWidth = rectangle.borderSize;
+			context.strokeStyle = rectangle.borderColor;
+			context.stroke();
+		}
+
+		if (rectangle.fillColor !== Colors.None) {
+			context.fillStyle = rectangle.fillColor;
+			context.fill();
+		}
 	}
+
 }
